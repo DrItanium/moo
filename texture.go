ef4bd5d4435a932e27658c611f9e3400ba3f3df5
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-07-05 22:50:34 -0700

    Started implementing textures.go and began to sigh at the hacks done

--- texture.go
+++ texture.go
@@ -2,6 +2,10 @@
 
 package moo
 
+import (
+	"github.com/DrItanium/moo/cseries"
+)
+
 type Pixel8 byte
 type Pixel16 uint16
 type Pixel32 uint32
@@ -63,3 +67,130 @@ func (this Pixel32) Green() byte {
 func (this Pixel32) Blue() byte {
 	return byte(this) & Pixel32MaximumComponent
 }
+
+func NewPixel32(r, g, b byte) Pixel32 {
+	rComponent := Pixel32(r) << 16
+	gComponent := Pixel32(g) << 8
+	bComponent := Pixel32(b)
+	return rComponent | gComponent | bComponent
+}
+
+type RgbColor struct {
+	Red   cseries.Word
+	Green cseries.Word
+	Blue  cseries.Word
+}
+
+type ColorTable struct {
+	Count  int16
+	Colors [256]RgbColor
+}
+
+// bitmap flags
+const (
+	ColumnOrderBit = 0x8000
+	TransparentBit = 0x4000
+)
+
+type BitmapDefinition struct {
+	Width, Height int16
+	BytesPerRow   int16
+	Flags         uint16
+	BitDepth      int16
+	// unused field
+	// unused [8]int16
+	RowAddresses [1]*Pixel8
+}
+
+// this function is wierd but before I explain it here is the c-code
+//pixel8 *calculate_bitmap_origin(
+//	struct bitmap_definition *bitmap)
+//{
+//	pixel8 *origin;
+//
+//	origin= (pixel8 *) (((byte *)bitmap) + sizeof(struct bitmap_definition));
+//	if (bitmap->flags&_COLUMN_ORDER_BIT)
+//	{
+//		origin+= bitmap->width*sizeof(pixel8 *);
+//	}
+//	else
+//	{
+//		origin+= bitmap->height*sizeof(pixel8 *);
+//	}
+//
+//	return origin;
+//}
+// So it looks like the objective is to compute the start of the given bitmap.
+// Initially the origin is address of bitmap.RowAddresses in memory.
+// If the ColumnOrderBit is set for bitmap then add the width of the bitmap times the sizeof a pixel8 pointer to origin
+// If the ColumnOrderBit is not set for the bitmap then add the height of the bitmap times the sizeof a pixel8 pointer to origin
+// return origin
+//
+// So following the bitmap structure itself the bitmap associated with the given header is found (encoding yay!)
+// The origin is defined as the pixel furthest away from the start of the bitmap data
+// There is also a comment found in textures.h attached to calculate_bitmap_origin which says:
+// /* assumes pixel data follows bitmap_definition structure immediately */
+// This proves my assumption of how this is laid out
+//
+//func (this *BitmapDefinition) CalculateBitmapOrigin() *Pixel8 {
+//	var origin *Pixel8
+//	origin =
+//}
+func (this *BitmapDefinition) ColumnOrder() bool {
+	return (this.Flags & ColumnOrderBit) != 0
+}
+func (this *BitmapDefinition) Transparent() bool {
+	return (this.Flags & TransparentBit) != 0
+}
+
+var Marathon1 = marathon1_game{}
+var Marathon2 = marathon2_game{}
+
+type marathon1_game struct{}
+type marathon2_game struct{}
+
+func (marathon1_game) IsMarathon() bool {
+	return true
+}
+
+func (marathon2_game) IsMarathon() bool {
+	return false
+}
+
+type Game interface {
+	IsMarathon1() bool
+}
+
+// comment from original code: "must initialize bytes_per_row, height, and row_address[0]"
+func (this *BitmapDefinition) PrecalculateBitmapRowAddresses(game Game) {
+	var rows int16
+	if this.ColumnOrder() {
+		rows = this.Width
+	} else {
+		rows = this.Height
+	}
+	//rowAddress := this.RowAddresses[0]
+	//table := this.RowAddresses
+	bytesPerRow := this.BytesPerRow
+	if bytesPerRow != cseries.None {
+		for row := int16(0); row < rows; row++ {
+			//*table++ = row_address; // WHY??? This is ambiguous
+			// should be
+			// table++;
+			// *table = row_address;
+		}
+	} else {
+		if game.IsMarathon1() {
+			// put code here from textures.c
+		} else {
+			// put code here from textures.c
+		}
+	}
+}
+
+/*
+void map_bytes(byte *buffer, byte *table, long size);
+void remap_bitmap(struct bitmap_definition *bitmap,	pixel8 *table);
+
+void erase_bitmap(struct bitmap_definition *bitmap, long pel);
+*/
