131a05a2d9b92767171660ab28be40d7a8c5635b
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-07-05 21:26:17 -0700

    Implemented Fixed functions from cseries.go

--- cseries/cseries.go
+++ cseries/cseries.go
@@ -9,6 +9,8 @@ const (
 	MachineTicksPerSecond = 60
 )
 
+type Word uint16
+
 func Signum(x int) int {
 	if x < 0 {
 		return -1
@@ -41,4 +43,32 @@ func Max(x, y int) int {
 	} else {
 		return y
 	}
+}
+
+type Fixed int32
+
+const (
+	FixedFractionalBits       = 16
+	FixedOne            Fixed = Fixed(1 << FixedFractionalBits)
+	FixedOneHalf        Fixed = Fixed(1 << (FixedFractionalBits - 1))
+)
+
+func (this Fixed) Float() float64 {
+	return (float64(this) / float64(FixedOne))
+}
+
+func FloatToFixed(f float64) Fixed {
+	return Fixed(f) * FixedOne
+}
+
+func IntegerToFixed(s int) Fixed {
+	return Fixed(s) << FixedFractionalBits
+}
+
+func (this Fixed) IntegralPart() int16 {
+	return int16(this >> FixedFractionalBits)
+}
+
+func (this Fixed) FractionalPart() int16 {
+	return int16(Fixed(int16(this)) & (FixedOne - 1))
 }
