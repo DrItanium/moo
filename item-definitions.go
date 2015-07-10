b54762447ebb5fee3b5343f3942f957c1cc3fc88
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-07-10 13:38:45 -0700

    Defined item-definitions.go

---
+++ item-definitions.go
@@ -0,0 +1,12 @@
+package moo
+
+type ItemDefinition struct {
+	ItemKind              int16
+	SingularNameId        int16
+	PluralNameId          int16
+	BaseShape             ShapeDescriptor
+	MaximumCountPerPlayer int16
+	InvalidEnvironments   int16
+}
+
+var ItemDefinitions []ItemDefinition
