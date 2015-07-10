ae372492080a1c03b6f3ed74706fa61b3e17b0a6
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-07-10 12:35:28 -0700

    Added assert.go to cseries

---
+++ cseries/assert.go
@@ -0,0 +1,13 @@
+// assertion emulation through the error type
+package cseries
+
+import "fmt"
+
+type AssertionError struct {
+	Function string
+	Message  string
+}
+
+func (this AssertionError) Error() string {
+	return fmt.Sprintf("%s: %s", this.Function, this.Message)
+}
