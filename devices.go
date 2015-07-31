a9270bfdb2bfed326ed11671d3561378bf2da018
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-07-30 21:21:04 -0700

    Added devices.go

---
+++ devices.go
@@ -0,0 +1,11 @@
+// code relating to ingame devices
+package moo
+
+import "github.com/DrItanium/moo/cseries"
+
+const (
+	OxygenRechargeFrequency = 0
+	EnergyRechargeFrequency = 0
+
+	MaximumActivationRange = 3
+)
