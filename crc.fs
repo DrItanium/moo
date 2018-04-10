b68813c12abff88e4d9be7bc6d085ecea10750a7
Josh Scoggins <jscoggins@parasoft.com>
2018-04-10 14:39:47 -0700

    Added blanks for other file implmentations

---
+++ crc.fs
@@ -0,0 +1,4 @@
+\ crc.fs - implementation of marathon infinity's Cyclic Redundancy Check definition
+
+
+;s \ must always be last thing in the file
---
+++ marathon2.fs
@@ -0,0 +1,6 @@
+\ marathon2.fs - moo's marathon2.c reimplementation
+
+
+
+
+;s \ must always be last thing in file
